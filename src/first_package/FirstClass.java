1bf1cb5c5b810737652ae8fbdefb5eb46c2104cc
Liliana <liliana473@yandex.ru>
2023-01-07 14:29:58 -0500

    update

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -2,4 +2,7 @@ package first_package;
 
 public class FirstClass {
 
+    public static void main(String[] args) {
+        System.out.println("First class");
+    }
 }
--- src/first_package/SecondClass.java
+++ src/first_package/SecondClass.java
@@ -2,4 +2,7 @@ package first_package;
 
 public class SecondClass {
 
+    public static void main(String[] args) {
+        System.out.println("Second class");
+    }
 }
